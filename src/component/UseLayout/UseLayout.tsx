114a9fb59ab440edef3923c715d9e34c8dc2ba36
SpairCode <1598111569@qq.com>
2019-08-12 10:46:04 +0800

    add zIndex

--- src/component/UseLayout/UseLayout.tsx
+++ src/component/UseLayout/UseLayout.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './UseLayout.less';
 import { Layout, Menu, Icon } from 'antd';
 import LeftNav from '../LeftNav/LeftNav';
 
-const {Header, Sider, Content } = Layout;
+const {Header, Sider, Content, Footer } = Layout;
 
 class UseLayout extends React.Component {
 
@@ -25,16 +26,13 @@ class UseLayout extends React.Component {
           <LeftNav/>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', lineHeight: '64px', textAlign: 'left', fontSize: '26px' }}>
-            <Icon
-              className="trigger"
-              type={collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toggle}
-            />
+          <Header style={{ background: '#fff', zIndex: 1 }}>
+            <Icon className={styles.trigger} type={collapsed ? `menu-unfold` : `menu-fold`} onClick={this.toggle}/>
           </Header>
           <Content style={{margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,}}>
             Content
           </Content>
+          <Footer style={{ textAlign: 'center' }}> © 2019 SpairFan@126.com </Footer>
         </Layout>
       </Layout>
     )
