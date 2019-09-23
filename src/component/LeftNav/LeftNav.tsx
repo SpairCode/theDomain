a0cab272c5350883399535a7c540904d61cf1536
SpairCode <1598111569@qq.com>
2019-09-23 16:35:32 +0800

    add router test

--- src/component/LeftNav/LeftNav.tsx
+++ src/component/LeftNav/LeftNav.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { connect } from 'dva';
 import menuList from '../../../config/menu.js';
 import { Menu, Icon } from 'antd';
 import styles from './LeftNav.less';
@@ -7,81 +6,47 @@ import { withRouter } from "react-router";
 const { SubMenu } = Menu;
 import router from 'umi/router'
 
-@connect(({ routes }) => ({ routes }))
-
 class LeftNav extends React.Component {
 
-  state = {
-    path: '',
-    openKey: ''
-  }
-
-  componentDidMount () {
-    const { dispatch } = this.props
-    dispatch({
-      type: 'routes/fetch',
-      payload: menuList
-    }).then(() => {
-      this.setState({
-        menu: this.renderLeft(this.props.routes.menu),
-        path: this.props.location.pathname,
-        openKey: '/' + this.props.location.pathname.split('/')[1]
-      })
-    })
-  }
-
-  componentDidUpdate (prevProps) {
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.setState({
-        menu: this.renderLeft(this.props.routes.menu),
-        path: prevProps.location.pathname,
-        openKey: '/' + prevProps.location.pathname.split('/')[1]
+  renderLeft = (menuLists) => {
+    if (typeof menuList !== 'function') {
+      let path = this.props.location.pathname
+      return menuLists.map(item => {
+        if (item.children) {
+          const cItem = item.children.find(items => path.indexOf(items.url) === 0)
+          if (cItem) {
+            this.openKeys = item.url
+            this.path = cItem.url
+          }
+          return <SubMenu key={item.url} title={<span> <Icon type={item.icon} /> <span> {item.title} </span> </span>}>{this.renderLeft(item.children)}</SubMenu>
+        } else {
+          if (item.icon) {
+            return <Menu.Item onClick={this.url.bind(this, item.url)} key={item.url}> <Icon type={item.icon} /> <span> {item.title} </span> </Menu.Item>
+          } else {
+            return <Menu.Item onClick={this.url.bind(this, item.url)} key={item.url}> <span> {item.title} </span> </Menu.Item>
+          }
+        }
       })
     }
   }
 
-  url = (path, id, name) => {
+  url = (path) => {
     router.push({
-      pathname: path,
-    })
-    this.setState({
-      path: path
-    })
-  }
-
-  renderLeft = (menuLists) => { 
-    return menuLists.map(item => {
-      if (item.children) {
-        // const cItem = item.children.find(item => item.url === this.state.path)
-        // if (cItem) {
-        //   this.openKeys = "'" + item.url + "'" 
-        //   console.log('this.openKeys:', this.openKeys)
-        // }
-        return <SubMenu key={item.url} title={<span> <Icon type={item.icon} /> <span> {item.title} </span> </span>}>{this.renderLeft(item.children)}</SubMenu>
-      } else {
-        if (item.icon) {
-          return <Menu.Item onClick={this.url.bind(this, item.url, item.id)} key={item.url}> <Icon type={item.icon} /> <span> {item.title} </span> </Menu.Item>
-        } else {
-          return <Menu.Item onClick={this.url.bind(this, item.url, item.id)} key={item.url}> <span> {item.title} </span> </Menu.Item>
-        }
-      }
+      pathname: path
     })
   }
 
   render () {
-    const menuNode = this.renderLeft(this.props.routes.menu)
-    const { path } = this.state
-    const openkeys = this.state.openKey
+    const menuLists = this.renderLeft(menuList)
     return (
       <div className={styles.leftNav}>
         <Menu
-          selectedKeys={[path]}
-          defaultOpenKeys={[openkeys]}
+          selectedKeys={[this.path]}
+          defaultOpenKeys={[this.openKeys]}
           mode="inline"
           theme="light"
-          // inlineCollapsed={collapsed}
         >
-          {menuNode}
+          {menuLists}
         </Menu>
       </div>
     )
